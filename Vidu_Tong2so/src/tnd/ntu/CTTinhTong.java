d7dfc262b29e671064eb6799e922e1c158dee66b
Trần Ngọc Duy <duy.tn.63cntt@ntu.edu.vn>
2025-02-14 20:36:46 +0700

    ViDu Tổng 2 Số
    Update ví dụ trên lớp

---
+++ Vidu_Tong2so/src/tnd/ntu/CTTinhTong.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package tnd.ntu;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class CTTinhTong {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//In ra 1 dòng tiêu đề App
+		System.out.print("CT TINH TONG \n");
+		
+		//Chuẩn bị cho việc nhập
+		Scanner banPhim = new Scanner(System.in);
+		
+		//In dòng huong nhan moi nhập A
+		System.out.println("Mời nhập a: ");
+		double a = banPhim.nextDouble();
+		System.out.println("Mời nhập b: ");
+		double b = banPhim.nextDouble();
+
+		//Tính toán 
+		double Tong = a + b;
+		
+		//In ra kết quả
+		System.out.print("Tong la : ");
+		System.out.print(Tong);
+		
+	}
+
+}
