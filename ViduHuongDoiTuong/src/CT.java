d323e68ec47059e73276ea7e9040903e24200ab2
Trần Ngọc Duy <duy.tn.63cntt@ntu.edu.vn>
2025-02-18 09:23:52 +0700

    Update Java HDT
    Đầy đủ theo video

--- ViduHuongDoiTuong/src/CT.java
+++ ViduHuongDoiTuong/src/CT.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 
 public class CT {
 
 	public static void main(String[] args) {
-		//Tạo ra 2 sản phẩm(hardcode)
+		/*//Tạo ra 2 sản phẩm(hardcode)
 		SanPham sp1 = new SanPham();
 		SanPham sp2 = new SanPham(2, "Bánh Chuối", "Thức Ăn Nhanh", "bc.jpg");
 		sp1.setMaSP( 1);
@@ -16,7 +17,32 @@ public class CT {
 			   thongtinSP2  += "\nTên SP2 là: " + sp2.getTenSP();
 			   
 		System.out.println(thongtinSP1);
-		System.out.println(thongtinSP2);
+		System.out.println(thongtinSP2); */
+		
+		//Khai báo danh sách sản phẩm
+		ArrayList<SanPham> dsSanPham;
+		//Xin mới
+		dsSanPham = new ArrayList<SanPham> ();
+		
+		//Nhập 3 sản phẩm
+//		SanPham sp1 = new SanPham(0, "Kem", "Giải Khát", null);
+		SanPham sp2 = new SanPham(1, "Cơm", "Đồ ăn", null);
+		SanPham sp3 = new SanPham(2, "Phở", "Đồ ăn", null);
+		
+		//Thêm vào ArryList
+		dsSanPham.add(new SanPham(0, "Kem", "Giải Khát", null));
+		dsSanPham.add(sp2);
+		dsSanPham.add(sp3);
+		
+//		for (SanPham x: dsSanPham)
+//			System.out.println(x.toString());
+//
+		for (int i = 0; i < dsSanPham.size(); i++) {
+			{
+				SanPham sp = dsSanPham.get(i);
+				System.out.println(sp.toString());
+			}
+		}
 		
 	}
 
